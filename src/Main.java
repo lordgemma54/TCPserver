c380f8cbb191c8079f2eae2006a9046f2fa92ddc
RTV <rtvconcepts@gmail.com>
2026-09-01 23:27:14 -0400

    change to java 8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main(String[] args) {
-    if (args.length != 2) {
-        throw new IllegalArgumentException("Incorrect number of params");
+public class Main {
+    public static void main(String[] args) {
+        String hostName;
+        int portNumber;
+
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Incorrect number of params");
+        }
     }
+
+
 }
