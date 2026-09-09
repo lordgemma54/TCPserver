590e4ab6787444bedaa12cc04560cfc121715c82
RTV <rtvconcepts@gmail.com>
2026-09-08 21:12:36 -0400

    real start

--- src/com/cpsc3350/networks/MyFirstTCPServer.java
+++ src/com/cpsc3350/networks/MyFirstTCPServer.java
@@ -5,6 +5,16 @@ import java.net.ServerSocket;
 
 public class MyFirstTCPServer {
 
+//    create parent socket
+//    bind port
+//    adjust queue limit if needed
+//    wait for an incoming request
+//    accept that incoming request and create child socket
+//    establish input and output streams
+//    read and write
+//    close child socket
+    
+
     public void methodName() throws IOException{
         try {
             ServerSocket server = new ServerSocket();
